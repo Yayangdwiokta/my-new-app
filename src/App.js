8709e7fa70dd9578567180035cd74a0c88efd6b0
Yayangdwiokta <yayangdwioktavia@gmail.com>
2022-03-28 10:50:57 +0700

    my-new-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './index.css';
-import Song from "./components/song";
+import './App.css';
+import Song from './components/song';
 
-import data from "./data/dataAlbum";
 function App() {
   return (
     <div className="App">
-      <form/>
-      <Song img={data.album.images[0].url} title={data.name} artist={data.artists[0].name}/>
+      <header className="App-header">
+        <Song/>
+        
+      </header>
     </div>
-  );
-}
+  )
+  
+};
 
 export default App;
